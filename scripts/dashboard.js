8dbde1b398245bf1447de8ebe8fe84a070491b7b
nthnn <nathanneisip@gmail.com>
2024-01-22 15:07:03 +0800

    Dashboard dataTables initializations.

---
+++ scripts/dashboard.js
@@ -0,0 +1,5 @@
+$(document).ready(()=> {
+    Librarium.initDataTable("#recent-data-table", "No recent transaction data found.");
+    Librarium.initDataTable("#books-table", "No books found.");
+    Librarium.initDataTable("#students-table", "No students found.");
+});
