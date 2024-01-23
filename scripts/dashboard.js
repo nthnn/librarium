8da52096573652f1cf17e86fc327fd730b88ada0
nthnn <nathanneisip@gmail.com>
2024-01-23 17:34:18 +0800

    Loading modal for buffer in dashboard page.

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,4 +1,7 @@
 $(document).ready(()=> {
+    $("#loading-modal").modal("show");
+    setTimeout(()=> $("#loading-modal").modal("hide"), 2000);
+
     Librarium.initDataTable("#recent-data-table", "No recent transaction data found.");
     Librarium.initDataTable("#books-table", "No books found.");
     Librarium.initDataTable("#students-table", "No students found.");
