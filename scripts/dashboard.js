313226f47834fcab0a26efe0f90ec9f740790000
nthnn <nathanneisip@gmail.com>
2024-01-23 16:24:43 +0800

    Functional webcam listing and preview of scanner.

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -2,4 +2,6 @@ $(document).ready(()=> {
     Librarium.initDataTable("#recent-data-table", "No recent transaction data found.");
     Librarium.initDataTable("#books-table", "No books found.");
     Librarium.initDataTable("#students-table", "No students found.");
+
+    Librarium.startScanner(()=> {}, ()=> {});
 });
