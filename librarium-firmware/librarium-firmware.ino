738de8483b19bdc515ad7bb56cff3791e3e489d6
nthnn <nathanneisip@gmail.com>
2024-01-24 22:42:06 +0800

    Small code and identation fixes on firmware.

--- librarium-firmware/librarium-firmware.ino
+++ librarium-firmware/librarium-firmware.ino
@@ -1,30 +1,28 @@
-#define BUZZER 9
-#define LED_LEVEL_1 2
-#define LED_LEVEL_2 3
+#define LED_LEVEL_1_PIN     2
+#define LED_LEVEL_2_PIN     3
 
-#define LED_SUCCESS 4
-#define LED_FAILED 5
+#define SENSOR_ECHO_PIN     6
+#define SENSOR_TRIGGER_PIN  7
 
-#define SENSOR_ECHO 6
-#define SENSOR_TRIGGER 7
+#define BUZZER_PIN          9
 
 void singleBuzz();
 void doubleBuzz();
+
 void turnOffLed();
 void switchLed(uint8_t level);
+
 uint8_t detectObject();
 
 unsigned long currentMillis = 0UL;
 bool watchOut = false;
 
 void setup() {
-  pinMode(BUZZER, OUTPUT);
-
-  pinMode(LED_LEVEL_1, OUTPUT);
-  pinMode(LED_LEVEL_2, OUTPUT);
-
-  pinMode(SENSOR_TRIGGER, OUTPUT);
-  pinMode(SENSOR_ECHO, INPUT);
+  pinMode(LED_LEVEL_1_PIN,    OUTPUT);
+  pinMode(LED_LEVEL_2_PIN,    OUTPUT);
+  pinMode(BUZZER_PIN,         OUTPUT);
+  pinMode(SENSOR_TRIGGER_PIN, OUTPUT);
+  pinMode(SENSOR_ECHO_PIN,    INPUT);
 
   Serial.begin(9600);
   while(!Serial);
@@ -76,9 +74,9 @@ void loop() {
 }
 
 void singleBuzz() {
-  tone(BUZZER, 1150);
+  tone(BUZZER_PIN, 1150);
   delay(280);
-  noTone(BUZZER);
+  noTone(BUZZER_PIN);
 }
 
 void doubleBuzz() {
@@ -88,8 +86,8 @@ void doubleBuzz() {
 }
 
 void turnOffLed() {
-  digitalWrite(LED_LEVEL_1, LOW);
-  digitalWrite(LED_LEVEL_2, LOW);
+  digitalWrite(LED_LEVEL_1_PIN, LOW);
+  digitalWrite(LED_LEVEL_2_PIN, LOW);
 }
 
 void switchLed(uint8_t level) {
@@ -97,10 +95,10 @@ void switchLed(uint8_t level) {
 
   switch(level) {
     case 2:
-      digitalWrite(LED_LEVEL_2, HIGH);
+      digitalWrite(LED_LEVEL_2_PIN, HIGH);
 
     case 1:
-      digitalWrite(LED_LEVEL_1, HIGH);
+      digitalWrite(LED_LEVEL_1_PIN, HIGH);
 
       currentMillis = millis();
       watchOut = true;
@@ -115,13 +113,13 @@ void switchLed(uint8_t level) {
 }
 
 uint8_t detectObject() {
-  digitalWrite(SENSOR_TRIGGER, LOW);
+  digitalWrite(SENSOR_TRIGGER_PIN, LOW);
   delayMicroseconds(2);
-  digitalWrite(SENSOR_TRIGGER, HIGH);
+  digitalWrite(SENSOR_TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(SENSOR_TRIGGER, LOW);
+  digitalWrite(SENSOR_TRIGGER_PIN, LOW);
 
-  long duration = pulseIn(SENSOR_ECHO, HIGH);
+  long duration = pulseIn(SENSOR_ECHO_PIN, HIGH);
   uint8_t distance = duration / 29 / 2;
 
   return distance;
