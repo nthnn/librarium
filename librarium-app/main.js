bcdc5d567ed5e0a3a094e4bdd94b66cc46f19486
nthnn <nathanneisip@gmail.com>
2024-01-26 00:13:19 +0800

    Optimized main.js for Electron BrowserWindow.

--- librarium-app/main.js
+++ librarium-app/main.js
@@ -1,8 +1,7 @@
-const { app, BrowserWindow, globalShortcut, screen } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 require('@electron/remote/main').initialize();
 
 app.on('ready', ()=> {
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
     let mainWindow = new BrowserWindow({
         width: 1100,
         height: 700,
@@ -21,13 +20,16 @@ app.on('ready', ()=> {
         },
     });
 
-    globalShortcut.register("Control+Shift+I", () => { return false; });
-    globalShortcut.register("Control+R", () => { return false; });
-    globalShortcut.register("Control+W", () => { return false; });
-
-    globalShortcut.register("Command+Option+I", () => { return false; });
-    globalShortcut.register("Command+R", () => { return false; });
-    globalShortcut.register("Command+W", () => { return false; });
+    if(process.platform == "darwin") {
+        globalShortcut.register("Command+Option+I", () => { return false; });
+        globalShortcut.register("Command+R", () => { return false; });
+        globalShortcut.register("Command+W", () => { return false; });
+    }
+    else {
+        globalShortcut.register("Control+Shift+I", () => { return false; });
+        globalShortcut.register("Control+R", () => { return false; });
+        globalShortcut.register("Control+W", () => { return false; });
+    }
 
     mainWindow.loadFile("index.html");
     mainWindow.on("closed", () => mainWindow = null);
